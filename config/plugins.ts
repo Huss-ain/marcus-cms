ebf94ef38a3f797ddb9b1e1a902b9b280dcfec62
Huss-ain <151734377+Huss-ain@users.noreply.github.com>
2025-06-11 14:21:59 +0100

    added public url

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,24 @@
 export default ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'aws-s3',
-      providerOptions: {
-        s3Options: {
-          credentials: {
-            accessKeyId: env('R2_ACCESS_KEY_ID'),
-            secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+    upload: {
+      config: {
+        provider: 'aws-s3',
+        providerOptions: {
+          s3Options: {
+            credentials: {
+              accessKeyId: env('R2_ACCESS_KEY_ID'),
+              secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
+            },
+            endpoint: env('R2_ENDPOINT'),
+            region: 'auto',
+            signatureVersion: 'v4',
+            forcePathStyle: true,
           },
-          endpoint: env('R2_ENDPOINT'),
-          region: 'auto',
-          signatureVersion: 'v4',
-          forcePathStyle: true, // This is the new key for AWS SDK v3
-        },
-        params: {
-          Bucket: env('R2_BUCKET'),
+          params: {
+            Bucket: env('R2_BUCKET'),
+          },
+          // ADD THIS LINE:
+          baseUrl: 'https://pub-f4707cbe468245578d759d0602fa68f0.r2.dev', // <-- your public R2 URL
         },
       },
     },
-  },
-});
+  });
