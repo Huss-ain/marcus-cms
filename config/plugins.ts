f9ee8f356bdb01d57f9d8ebb9be0147b2bbbaa33
Huss-ain <151734377+Huss-ain@users.noreply.github.com>
2025-06-11 13:15:30 +0100

    still trying

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,20 +1,14 @@
-export default ({ env }) => {
-    console.log('ALL ENV:', process.env);
-    console.log('BUCKET:', env('R2_BUCKET'));
-    console.log('PROVIDER:', require('strapi-provider-cloudflare-r2'));
-    
-    return {
-      upload: {
-        config: {
-          provider: 'strapi-provider-cloudflare-r2',
-          providerOptions: {
-            accountId: env('R2_ACCOUNT_ID'),
-            accessKey: env('R2_ACCESS_KEY_ID'),      // <-- CHANGE HERE
-            secretKey: env('R2_SECRET_ACCESS_KEY'),  // <-- CHANGE HERE
-            bucket: env('R2_BUCKET'),
-            publicUrl: env('R2_PUBLIC_URL'),
-          },
+export default ({ env }) => ({
+    upload: {
+      config: {
+        provider: 'strapi-provider-cloudflare-r2',
+        providerOptions: {
+          accountId: env('R2_ACCOUNT_ID'),
+          accessKeyId: env('R2_ACCESS_KEY_ID'),         // <-- v0.3.0 expects these
+          secretAccessKey: env('R2_SECRET_ACCESS_KEY'), // <-- v0.3.0 expects these
+          bucket: env('R2_BUCKET'),
+          publicUrl: env('R2_PUBLIC_URL'),
         },
       },
-    };
-  };
+    },
+  });
