8a5caf4f330d4023cf718fb16a515ec5b810ede9
Huss-ain <151734377+Huss-ain@users.noreply.github.com>
2025-06-11 12:34:22 +0100

    changed accesskey and secretkey

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,19 +1,19 @@
 export default ({ env }) => {
-  console.log('ALL ENV:', process.env);
-  console.log('BUCKET:', env('R2_BUCKET'));
-  
-  return {
-    upload: {
-      config: {
-        provider: 'strapi-provider-cloudflare-r2',
-        providerOptions: {
-          accountId: env('R2_ACCOUNT_ID'),
-          accessKeyId: env('R2_ACCESS_KEY_ID'),
-          secretAccessKey: env('R2_SECRET_ACCESS_KEY'),
-          bucket: env('R2_BUCKET'),
-          publicUrl: env('R2_PUBLIC_URL'),
+    console.log('ALL ENV:', process.env);
+    console.log('BUCKET:', env('R2_BUCKET'));
+    
+    return {
+      upload: {
+        config: {
+          provider: 'strapi-provider-cloudflare-r2',
+          providerOptions: {
+            accountId: env('R2_ACCOUNT_ID'),
+            accessKey: env('R2_ACCESS_KEY_ID'),      // <-- CHANGE HERE
+            secretKey: env('R2_SECRET_ACCESS_KEY'),  // <-- CHANGE HERE
+            bucket: env('R2_BUCKET'),
+            publicUrl: env('R2_PUBLIC_URL'),
+          },
         },
       },
-    },
+    };
   };
-};
